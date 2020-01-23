31c50033bf59525720f28e49aa1e915d49e6a32d
Jan Erik Bertram <JBertram.hays@dspace.de>
2020-01-23 13:30:14 +0100

    Changed echo functions to batch-macros The ASCII-ESC character is created by the PROMPT command Simplified the file structure Removed test code from echo_macros.bat
    The $echo_fatal macro expects the exitcode as first parameter instead of last one

--- test_colors.bat
+++ test_colors.bat
@@ -1,28 +1,52 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal Disabledelayedexpansion
 
-rem https://stackoverflow.com/questions/112055/what-does-d0-mean-in-a-windows-batch-file
-for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
-set script_dir=%script_dir%\..
-@echo %script_dir%
+REM *** Defining all macros, once
+call "%~dp0\echo_macros.bat"
 
-call %script_dir%\batcolors\echos_macros.bat
+call :test1
+call :test2
+call :test3
 
-set NOCOLORS=
+echo This line shouldn't be visible, because the last fatal error should exit the batch
+exit /b
+
+:test1
+echo Test1
+set "NOCOLORS="
 set FATALNOEXIT=1
-%_ok% "Result matches what was expected"
-%_info% "Describe what is about to be done"
-%_warning% "Result was not expected, but non-blocking"
-%_error% "Result is wrong"
-%_fatal% "Program must stop and exit" 1
+
+%$echo_ok% "Special chars &|<>^! Text"
+%$echo_info% "It should work with "quotes", too"
+%$echo_warning% "The percent sign %% has to be doubled"
+echo(
+exit /b
+
+:test2
+echo Test2
+set "NOCOLORS="
+set FATALNOEXIT=1
+%$define_prefixed_echo% my_colored_echo "43;30" "****"
+%my_colored_echo% "this is my own macro"
+echo(
+exit /b
+
+:test3
+echo Test3
+set "NOCOLORS="
+set FATALNOEXIT=1
+
+%$echo_ok% "Result matches what was expected"
+%$echo_info% "Describe what is about to be done"
+%$echo_warning% "Result was not expected, but non-blocking"
+%$echo_error% "Result is wrong"
+%$echo_fatal% 2 "Program must stop and exit"
 echo ---- NOCOLORS=1 ----
 set NOCOLORS=1
 set FATALNOEXIT=
-%_ok% "(no colors) Result matches what was expected"
-%_info% "(no colors) Describe what is about to be done"
-%_warning% "(no colors) Result was not expected, but non-blocking"
-%_error% "(no colors) Result is wrong"
-%_fatal% "(no colors) The program must exit and stop" 2
-
-REM final echo should not be displayed:
-echo All done
+%$echo_ok% "(no colors) Result matches what was expected"
+%$echo_info% "(no colors) Describe what is about to be done"
+%$echo_warning% "(no colors) Result was not expected, but non-blocking"
+%$echo_error% "(no colors) Result is wrong"
+%$echo_fatal% 4 "(no colors) The program must exit and stop"
+goto:eof
