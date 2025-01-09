ccd6afd811f9cf0e0c3db4ddbff48b2968b540e2
VonC <vonc@laposte.net>
2025-01-09 14:09:08 +0100

    feat(bat): echos_macros.bat adds _pre and _post macros

--- echos_macros.bat
+++ echos_macros.bat
@@ -26,6 +26,8 @@ set "_fatal=call:call_echos_stack 2>NUL & call "%%batdir%%\echos.bat" :fatal"
 set "_stack=call:call_echos_stack"
 set "_stack_call=call:call_echos_stack & call"
 set "_unstack=call "%%batdir%%\echos.bat" :unstack"
+set "_pre=call "%%batdir%%\echos.bat" :pre"
+set "_post=call "%%batdir%%\echos.bat" :post"
 
 set "CHECK_DEBUG_ECHOS=echo %DEBUG_ECHOS% | findstr /C:true >nul &&
 
